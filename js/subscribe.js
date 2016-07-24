e25088b7e56436a2ac3fb938220987eaa4386938
Max Bulygin <maksym.bulygin@gmail.com>
2016-07-24 22:38:31 +0300

    client side BASIC functionality DONE

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,59 +1,58 @@
-$(function(){
+$(function () {
     
-    //Implement form launcning on button click
+    var isNameValid = false,
+        isEmailValid = false,
+        isFormLoaded = false,
+        $overlay = $('<div class="overlay"></div>');
     
-    var isFormLoaded = false,
-        $overlay = $('<div class="overlay"></div>'),
-        $userName = $("#userName");
+    //Show/hide form on button click
     
-    $("#subscribe").click(function(){
-        if(!isFormLoaded){
+    $("#subscribe").click(function () {
+        if (!isFormLoaded) {
             $("body").append($overlay);
             $overlay.load('form.html');
             isFormLoaded = true;
-        } else {
-            $overlay.show();
-        }   
+        }
+        else {
+            $overlay.fadeIn();
+        }
     });
     
-    $("body").on("click", function(e){
-        if (e.target.className === "overlay")
-            $overlay.hide();
+    $("body").on("click", function (e) {
+        if (e.target.className === "overlay") $overlay.fadeOut();
     });
     
     //Form validation
     
-    var isNameValid = false,
-        isEmailValid = false;
-    
-    function checkName(){
-        if ( $(this).val().length === 0 ){
-            $(this).prev().show();
+    function checkName() {
+        if ($(this).val().length === 0) {
+            $(this).prev().fadeIn();
             isNameValid = false;
-        } else {
-            $(this).prev().hide();
+        }
+        else {
+            $(this).prev().fadeOut();
             isNameValid = true;
         }
     }
-    
+
     function checkEmail() {
         var re = /^[^\W][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z]{2,4}$/;
         var email = $(this).val();
-        
-        if( email.search(re) === -1 ){
-            $(this).prev().show();
+        if (email.search(re) === -1) {
+            $(this).prev().fadeIn();
             isEmailValid = false;
-        } else {
-            $(this).prev().hide();
+        }
+        else {
+            $(this).prev().fadeOut();
             isEmailValid = true;
         }
     }
-    
-    function canSubmit(){
+
+    function canSubmit() {
         return isNameValid && isEmailValid;
     }
-    
-    function enableSubmit(){
+
+    function enableSubmit() {
         $("input[type=submit]").prop("disabled", !canSubmit());
     }
     
@@ -62,20 +61,20 @@ $(function(){
     
     //Send data to the server
     
-    $("#signUpForm").submit(function(e){
-        e.preventDefault();
+    $("body").on('submit', "#signUpForm", function (e) {
+            e.preventDefault();
         
-        var formUrl = $(this).attr("action"),
-            formData = $(this).serialize(); //collect data from inputs
+            var formUrl = $(this).attr("action"),
+                formData = $(this).serialize(); //collect data from inputs
         
-        $.post(formUrl, formData, function(){
-            $("#signUpForm").html('<p class="success">Спасибо за подписку!</p>');
-            $(".overlay").delay(1500).fadeOut();
-        }).fail(function(jqXHR){
-            var $message = '<p class="fail">Что-то пошло не так. Попробуйте еще раз позже!</p>';
-            $message += '<p class="fail">' + jqXHR.statusText + '</p>';
-            $("#signUpForm").html($message);
-            $(".overlay").delay(1500).fadeOut();
-        })
-    }) 
+            $.post(formUrl, formData, function () {
+                    $("#signUpForm").html('<p class="success">Спасибо за подписку!</p>');
+                    $(".overlay").delay(1500).fadeOut();
+                }).fail(function (jqXHR) {
+                    var $message = '<p class="fail">Что-то пошло не так. Попробуйте еще раз позже!</p>';
+                    $message += '<p class="fail">' + jqXHR.statusText + '</p>';
+                    $("#signUpForm").html($message);
+                    $(".overlay").delay(1500).fadeOut();
+                }) //end fail
+        }) //end on 
 });
