b084dde1889c0449c6bd430322863274ff99c502
Max Bulygin <maksym.bulygin@gmail.com>
2016-07-27 01:57:23 +0300

    writing to file Name,Email,IP,Date DONE

--- write-csv.php
+++ write-csv.php
@@ -1,5 +1,9 @@
 <?php
-    $my_file = 'subscribers.csv';
-    $handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
-    $data = "\n" . $_REQUEST['name'] . ", " . $_REQUEST['email'];
-    fwrite($handle, $data);
+    $my_file    = 'users/subscribers.csv';
+    $handle     = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+    $ip         = $_SERVER["REMOTE_ADDR"];
+    $name       = $_REQUEST['name'];
+    $email      = $_REQUEST['email'];
+    $date       = date("Y/m/d"); 
+    $message    = "\n" . $name . "," . $email . "," . $ip . "," . $date;
+    fwrite($handle, $message);
